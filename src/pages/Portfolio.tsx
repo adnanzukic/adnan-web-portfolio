7efdf531b922d6d8d1bb33faea3c84148b215f77
Adnan <adnan16zukic@gmail.com>
2026-03-09 23:39:05 +0100

    Update Portfolio.tsx

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -7,7 +7,7 @@ import projectEcommerce from "@/assets/project-ecommerce.jpg";
 
 const projects = [
   { img: projectRestaurant, title: "Restaurant Website Concept", category: "Web Design" },
-  { img: "/slike/gym.preview.jpg", title: "Gym Website Concept", category: "Web Design" },
+  { img: "/slike/gymweb.jpg", title: "Gym Website Concept", category: "Web Design" },
   { img: projectLawfirm, title: "Law Firm Website Concept", category: "Corporate" },
   { img: projectEcommerce, title: "Online Store Concept", category: "E-commerce" },
 ];
